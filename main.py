f94afe21cd21321da5dc89dab264dd47270381ba
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-03-19 12:38:12 +0300

    All_done

--- main.py
+++ main.py
@@ -30,31 +30,39 @@ def subs_phones():
     pattern = r"(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)" \
               r"(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)" \
               r"(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)"
+    new_pattern = r"+7(\4)\8-\11-\14\15\17\18\19\20"
     for people in contacts_list:
-        people[5] = re.sub(pattern, r"+7(\4)\8-\11-\14\15\17\18\19\20", people[5])
+        people[5] = re.sub(pattern, new_pattern, people[5])
     return contacts_list
 
 def delete_doubles():
-    new_list = []
+    new_dic = {}
     for contact in contacts_list:
-        for people in new_list:
-            if contact[0] == people[0] and contact[1] == people[1]:
-                pass
-            else:
-                new_list.append(contact)
-    return new_list
+        if f'{contact[0]} {contact[1]}' not in new_dic:
+            new_dic[f'{contact[0]} {contact[1]}'] = contact
+        else:
+            for i in range(0, len(contact)-1): # for i in range(0, len(contact)):
+                if new_dic[f'{contact[0]} {contact[1]}'][i] == '':
+                    new_dic[f'{contact[0]} {contact[1]}'][i] = contact[i]
+    phonebook = list(new_dic.values())
+    return phonebook
+
 
 def write_file():
     with open("phonebook.csv", "w", encoding='UTF-8') as f:
         datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows()
+        datawriter.writerows(phonebook)
 
 
 if __name__ == '__main__':
     contacts_list = read_file()
     structure_names()
     subs_phones()
-    pprint(contacts_list)
+    phonebook = delete_doubles()
+    write_file()
+
+
+
 
 
 
